532eeead360aae3aceb77cacf955178ee7d7ee19
Mark Kimball <parkskier@gmail.com>
2015-07-08 14:22:10 -0400

    styling, materials list now visible

--- store/store.controller.js
+++ store/store.controller.js
@@ -10,6 +10,7 @@
 
       if($routeParams.productId){
         StoreService.getItem($routeParams.productId).then(function(item){
+          console.log(item);
           $scope.item = item;
         })
       }
