f06eeecea4a5555119a211e1e979d07909b1e29f
Mark Kimball <parkskier@gmail.com>
2015-07-13 12:36:24 -0400

    added link event, transition

--- store/store.directive.js
+++ store/store.directive.js
@@ -8,6 +8,14 @@
         templateUrl: 'store/views/storeItem.html',
         scope: {
           i: '='
+        },
+        link: function(scope, element, attributes){
+          element.on('mouseenter', function(){
+            element.addClass('dropShadow');
+          });
+          element.on('mouseleave', function(){
+            element.removeClass('dropShadow');
+          });
         }
       }
     })
