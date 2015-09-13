370fdd36c75c271165a164ead7cabf27563fe325
Mark Kimball <parkskier@gmail.com>
2015-09-13 16:37:48 -0400

    color changes and added flex

--- shoppingcart/shoppingcart.controller.js
+++ shoppingcart/shoppingcart.controller.js
@@ -4,9 +4,9 @@
     .module('shoppingcart')
     .controller('ShoppingCartController', function($scope, _, ShoppingCartService, $location){
 
-        ShoppingCartService.getCart().then(function(items){
-          $scope.items = items;
-        })
+      ShoppingCartService.getCart().then(function(items){
+        $scope.items = items;
+      });
 
       $scope.orderTotalPrice = function(items){
         var total = 0;
@@ -14,15 +14,15 @@
           total += Number(el.price);
         });
         return total;
-      }
+      };
 
       $scope.addToCart = function(item){
         ShoppingCartService.addToCart(item);
-      }
+      };
 
       $scope.removeFromCart = function(id){
         ShoppingCartService.removeFromCart(id);
-      }
+      };
 
       var watchCallback = function () {
           ShoppingCartService.getCart().then(function (items) {
